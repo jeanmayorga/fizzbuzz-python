05c84b4ade69ce14d8e76044039e82be2dfb8cd6
Paul Mayorga <jpmayorga@outlook.com>
2021-04-03 13:31:21 -0500

    fix: adding autopep8

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 def fizzbuzz(number: int) -> int or str:
-  if (number == 0):
-    return 0
+    if (number == 0):
+        return 0
 
-  if (number % 3 == 0 and number % 5 == 0):
-    return 'fizzbuzz'
+    if (number % 3 == 0 and number % 5 == 0):
+        return 'fizzbuzz'
 
-  if (number % 3 == 0):
-    return 'fizz'
+    if (number % 3 == 0):
+        return 'fizz'
 
-  if (number % 5 == 0):
-    return 'buzz'
+    if (number % 5 == 0):
+        return 'buzz'
 
-  return number
+    return number
+
 
 def run_fizzbuzz(count: int) -> None:
-  for x in range(count):
-    print(f'number: {x}: {fizzbuzz(x)}')
+    for x in range(count):
+        print(f'number: {x}: {fizzbuzz(x)}')
+
 
 run_fizzbuzz(16)
